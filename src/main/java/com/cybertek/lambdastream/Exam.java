f690a901072f8eeb4539ca3a63ac3563008809d5
Liridon Morina <liridon@cyberktechschool.com>
2020-10-15 09:48:32 +0200

    add one more method converted duration to week

--- src/main/java/com/cybertek/lambdastream/Exam.java
+++ src/main/java/com/cybertek/lambdastream/Exam.java
@@ -4,8 +4,10 @@ import com.cybertek.lambdastream.model.CourseAssigned;
 import com.cybertek.lambdastream.model.CourseStatus;
 import com.cybertek.lambdastream.model.User;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Exam {
 
@@ -74,6 +76,20 @@ public class Exam {
      */
     public static List<Map<String, Object>> modifyObject() {
         return null;
+    }
+
+    /**
+     * Duration of course that is in hours return to week that specific user finished courses.
+     * One week has 40 hours working time. divider parameter should be 40.
+     * @return converted duration to week. {@link BigDecimal}
+     */
+    public static BigDecimal divideToWeek() {
+        User specificUser = DataGenerator.findUserById(6);
+        BigDecimal divideToWeek = new BigDecimal(40);
+        return null;
+    }
+
+    public static void main(String[] args) {
     }
 
 }
